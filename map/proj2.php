f21e34d582ca5127a589e12e3e7780e4e947820f
ads <amands145@gmail.com>
2016-11-18 18:27:37 -0500

    Added test Openlayers Map

--- map/proj2.php
+++ map/proj2.php
@@ -7,13 +7,15 @@ foreach ($_GET as $k=>$v) {
 }
 
 ms_ioinstallstdouttobuffer();
-$oMap = ms_newMapobj("/home/zhang/public_html/proj2/proj2.map");
+$oMap = ms_newMapobj("/home/sing16/public_html/proj2/proj2.map");
+
 
 $new_layer =ms_newlayerobj($oMap);
 $new_layer->set("type", MS_LAYER_POLYGON);
 $new_layer->set("dump", 1);
 $new_layer->set("status", 1);
 $new_layer->set("name","nypp");
+$new_layer->set("labelitem", "precinct");
 $new_layer->set("template","infotemplate.html");
 $new_layer->setMetaData("wms_name","nypp");
 $new_layer->setMetaData("wms_extent","913154.843600 120114.582600 1067382.510900 272932.046000");
@@ -22,12 +24,14 @@ $new_layer->setMetaData("gml_featureid","precinct");
 $new_layer->setMetaData("wms_feature_info_mime_type","text/html");
 $new_class = ms_newClassObj($new_layer);
 $new_style = ms_newStyleObj($new_class);
-$new_style-> outlinecolor->setRGB(255, 0, 0);
+$new_style-> color->setRGB(60,60,62);
+$new_style-> outlinecolor->setRGB(76,77,78);
 
 $new_layer->setConnectionType(MS_POSTGIS);
-$new_layer->set("connection","user=demo password=demo dbname=coursedemo host=192.168.88.30");
+$new_layer->set("connection","user=sing16 password=singhsingh dbname=d120 host=134.74.112.18");
 $data="geom from (select * from nypp order by precinct) as foo using unique precinct using SRID=2263";
 $new_layer->set("data",$data) ;
+
 
 $oMap->owsdispatch($request);
 $contenttype = ms_iostripstdoutbuffercontenttype();
